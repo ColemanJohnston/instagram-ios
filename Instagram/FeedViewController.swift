93dde393e8b1c003d941cec2195a5c7cd8f2e1f4
Coleman Johnston <john7117@m506-118-26434.local>
2018-02-27 00:49:57 -0800

    started pulling down posts from server and displaying them

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -7,20 +7,60 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var posts: [Post] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        fetchData()
         // Do any additional setup after loading the view.
     }
+    
+    func fetchData() {
+        // construct PFQuery
+        let query = Post.query()
+        query?.order(byDescending: "createdAt")
+        query?.includeKey("author")
+        query?.limit = 20
+        
+        // fetch data asynchronously
+        
+        
+        query?.findObjectsInBackground(block: {(posts, error) -> Void in
+            if let posts = posts {
+                for post in posts{
+                    print(post)
+                }
+                self.posts = posts as! [Post]
+                self.tableView.reloadData()
+            } else {
+                print(error?.localizedDescription)
+            }
+        })
 
+    }
+    
     @IBAction func didLogout(_ sender: Any) {
         print("logging out")
         NotificationCenter.default.post(name: NSNotification.Name("didLogout"), object: nil)
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as! PostTableViewCell
+        cell.instagramPost = posts[indexPath.row]
+        
+        return cell
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Coleman Johnston on 2/26/18.
+//  Copyright © 2018 Coleman Johnston. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: PFImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    var instagramPost: PFObject! {
+        didSet {
+            self.captionLabel.text = instagramPost["caption"] as? String
+            self.postImageView.file = instagramPost["image"] as? PFFile
+            self.postImageView.loadInBackground()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
