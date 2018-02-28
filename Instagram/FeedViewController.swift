00f4eb7a782e01c152e2ec1983e9dc8ca933723b
Coleman Johnston <john7117@m506-118-26434.local>
2018-02-27 22:52:03 -0800

    finished the details screen

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -77,15 +77,18 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if !(sender is UITableViewCell){
+            return
+        }
+        
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let post = posts[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.instagramPost = post
+        }
     }
-    */
+    
 
 }
